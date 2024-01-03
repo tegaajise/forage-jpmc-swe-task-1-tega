7151bbbaf6ea4f4471a8748b11b69a926160f5c5
Tega Ajise <tegaajise@college.harvard.edu>
2024-01-02 21:07:00 -0500

    task 1 complete

--- client_test.py
+++ client_test.py
@@ -8,6 +8,15 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    # Iterate through each quote in the list above
+    for quote in quotes:
+      # 
+      self.assertEqual(
+        # getDataPoint returns tuple of stock, top_bid_price, top_ask_price, stock_price_based_on_formula
+        getDataPoint(quote),
+        # need to form same tuple manually with each quote dictionary in the quotes list
+        # Things like top_ask and top_bid themselves return a dictionary
+      (quote["stock"], quote["top_bid"]["price"], quote["top_ask"]["price"], (quote["top_bid"]["price"] + quote["top_ask"]["price"])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
